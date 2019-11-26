817a2d61e881206e49cce081979a5d4d33aa0a21
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2019-11-25 22:12:04 -0500

    Initial binary configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name="dyer",
+    version="2019.1",
+    packages=['libdyer'],
+    #package_data={'dyer':['data/Makefile', 'data/*.xsl', 'data/*.sty']},
+    scripts=['bin/dyer'])
+        
