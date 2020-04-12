daae8bea659a36a8a56659eea5547d7664ea91af
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2020-04-11 20:05:00 -0400

    Save

--- setup.py
+++ setup.py
@@ -6,19 +6,25 @@ import re
 packages = [
     "libdyer",
     "libdyer.notify",
-    "libdyer.tasks"
+    "libdyer.tasks",
+    "libdyer.build",
+    "libdyer.meetup"
 ]
 package_dirs = {}
 for package in packages:
     package_dirs[package] = re.sub("\.", "/", package)
 
+scripts = ['dyer', 'mup']
+script_list = []
+for i in scripts:
+    script_list.append(f"scripts/{i}")
 
 
 setup(
     name="libdyer",
     version="2019.4",
     #package_data={'dyer':['data/Makefile', 'data/*.xsl', 'data/*.sty']},
-    scripts=['scripts/dyer'],
+    scripts=script_list,
     packages=packages,
     package_dir=package_dirs
     #packages=["libdyer", "libdyer.notify"],
